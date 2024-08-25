853bfc7670e7e6a2cfd08778790569773eb3fcdb
Abdi Rahman <abdirinoahash@gmail.com>
2024-08-25 14:40:22 +0300

    canceled

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Card from "@/Components/Card";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen mx-auto max-w-[100rem]">
-      
+      <Card />
     </main>
   );
 }
